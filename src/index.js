ef09b6a527b11493e90f6d1c0728d960b3fdbefc
Jakkanda <51709348+Jakkanda@users.noreply.github.com>
2021-04-25 17:11:06 +0300

    Project finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import menuItemsTemplate from './template.hbs';
 import menu from './menu.json';
 
+const darkTheme = 'dark-theme';
+
 const menuItemsList = document.querySelector('.js-menu');
 const markup = menuItemsTemplate(menu);
 
 menuItemsList.insertAdjacentHTML('beforeend', markup);
+
+const switchInput = document.querySelector('#theme-switch-toggle');
+
+const bodyPage = document.querySelector('body');
+
+switchInput.addEventListener('change', event => {
+  bodyPage.classList.toggle(darkTheme);
+  const isDarkTheme = bodyPage.classList.contains(darkTheme);
+  localStorage.setItem(darkTheme, isDarkTheme);
+});
+
+window.onload = () => {
+  setDarkTheme();
+};
+
+function setDarkTheme() {
+  const isDarkTheme = localStorage.getItem(darkTheme) === 'true';
+
+  if (isDarkTheme) {
+    bodyPage.classList.toggle(darkTheme);
+    switchInput.checked = isDarkTheme;
+  }
+}
